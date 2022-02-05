dda880d0e375fc469c925421246fec12a098c566
Saran Nonkamjan <saran.non@gmail.com>
2022-02-05 12:47:56 +0700

    aa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,10 +5,10 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "35a2273e70e8ec566eaf7f6d1c0e695e",
 "favicon.ico": "d80f8e834ab96ea8b0237cc4d559df24",
-"index.html": "685980b76cc4c6fa8cb6e41ff2594724",
-"/": "685980b76cc4c6fa8cb6e41ff2594724",
+"index.html": "49cd91f141c04fed5870dd62d2a7af24",
+"/": "49cd91f141c04fed5870dd62d2a7af24",
 "styles.css": "3bcb2ce7b2f079b344ab1b0230e13321",
-"main.dart.js": "7c824918b586ff1b8bbef42fbf5c4448",
+"main.dart.js": "e7f6cfadda5599f14c72a93066b18df9",
 "img/Neat-Tools-Banner-2.png": "326df15f80cd7f7057edd44a9828e0d7",
 "favicon.png": "a16d6f835695c0aadaa9b4f95b7a2e6f",
 "icons/Icon-192.png": "c913da70ec823fb227e3b5de0a7d8bd3",
